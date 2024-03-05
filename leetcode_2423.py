972d927a4e1ce7d9c7e14fd962b455e190727fd2
M-AAP <160674596+M-AAP@users.noreply.github.com>
2024-03-05 01:24:47 +0000

    Update leetcode_2423.py

--- leetcode_2423.py
+++ leetcode_2423.py
@@ -1,28 +1,30 @@
-def is_eq_freq(word):
-    letter_dict = {}
+def is_eq_freq(word): # a function which tells you if each letter in the word has the same frequency
+    letter_freq_dict = {} # Create a dictionary which will store the frequency of each letter in the word
     for letter in word:
-        if letter in letter_dict:
-            letter_dict[letter] += 1
+        if letter in letter_freq_dict:
+            letter_freq_dict[letter] += 1
         else:
-            letter_dict[letter] = 1
-    lowest = min(letter_dict, key=letter_dict.get)
-    highest = max(letter_dict, key=letter_dict.get)
+            letter_freq_dict[letter] = 1
+    lowest = min(letter_freq_dict, key=letter_freq_dict.get)
+    highest = max(letter_freq_dict, key=letter_freq_dict.get)
 
-    return letter_dict[lowest] == letter_dict[highest]
+    return letter_freq_dict[lowest] == letter_freq_dict[highest] 
+'''    
+If the smallest and largest values of the dict are equal then all values must be the same
+I.e. each letter has the same frequency
+'''
 
-def equalFrequency(self, word):
-    word_length = len(word)
-    removed_list = []
-    for i in range(len(word)):
-        removed = word.replace(word[i],"",1)
-        removed_list.append(removed)
+class Solution(object):
+    def equalFrequency(self, word):
+        word_length = len(word)
+        removed_list = [] # create a list of all the possible combinations of removing one letter from the word
+        for i in range(word_length):
+            removed = word.replace(word[i],"",1)
+            removed_list.append(removed)
 
-    is_eq_freq_list = map(is_eq_freq, removed_list)
+        is_eq_freq_list = map(is_eq_freq, removed_list) # a list containing whether each word has equal frequency or not
 
-    if True in is_eq_freq_list:
-        return True
-    else:
-        return False
-    
-            
-        
+        if True in is_eq_freq_list: # if even one of those words has all letters of equal frequency then return True and False otherwise 
+            return True
+        else:
+            return False
